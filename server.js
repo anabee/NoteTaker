409ebead93cbf1412befa1fc6ad28b57aaa1b17e
Ana Valdivia <anav1317@yahoo.com>
2020-03-17 17:00:37 -0700

    Added new middleware to fix issue

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ var db = require("./db/db.json");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 9000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 // =============================================================
@@ -26,16 +27,27 @@ app.get("/notes", function(req, res) {
 
 app.get("/api/notes", function(req, res) {
     // should read the db.json file
-    return res.json(db);
+    console.log(db)
+    res.json(db);
 });
 
 app.post("/api/notes", function(req, res) {
     // should receive a new note to save to the request body, add to db.json and return to the client 
-    console.log(res);
+    // console.log(res);
+    var newNote = req.body;
+
+    db.push(newNote);
+
+    res.json(newNote);
+
+    return res.json(db);
 });
 
 app.get("/api/notes/:id", function(req, res) {
     // should delete the notes based on their id 
+    var chose = req.params.id;
+
+    
 });
 
 app.get("*", function(req, res) {
